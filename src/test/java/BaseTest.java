1496001fac207adb490441a9622277de4fcfa8af
Kirill Demidov <demidovka20@yandex.ru>
2022-10-10 19:39:23 +0300

    lesson one

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+import static org.testng.Assert.fail;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    protected String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+    private JavascriptExecutor js;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        driver = new ChromeDriver();
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        js = (JavascriptExecutor) driver;
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+}
---
+++ src/test/java/OpenBrowserTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.*;
+
+    public class OpenBrowserTest extends BaseTest {
+
+        @Test
+        public void testAddRequest() throws Exception {
+            driver.get(baseUrl);
+        }
+    }
